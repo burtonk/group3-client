a54564d147e82ed5622ce08b39da93ff4c081437
Niamh Lawlor <lawlorni@Niamhs-MacBook-Pro.local>
2013-03-30 22:11:20 +0000

    sends info to check.php

---
+++ checkout.php
@@ -0,0 +1,83 @@
+<?php
+include 'cart.class.php';
+session_start();
+header('Content-Type: text/html; charset=utf-8');
+?>
+
+<html>
+	<head>
+		<title>Customer site</title>
+		<link rel="stylesheet" href="style.css" />
+		
+		<script src="js/prototype.js" language="JavaScript" type="text/javascript"></script>
+		<script src="js/jquery-1.9.1.min.js" language="JavaScript" type="text/javascript"></script>
+<script src="js/jquery-1.9.1.min.js" language="JavaScript" type="text/javascript"></script>
+<script type="text/javascript" language="JavaScript">
+function manageCart(task,item,price,name) {
+   var url = 'managecart.php';
+   var params = 'task=' + task + '&item=' + item + '&price=' + price + '&name=' + name;
+   var ajax = new Ajax.Updater(
+	          {success: 'cartResult'},
+              url,
+              {method: 'get', parameters: params, onFailure: reportError});
+			  $("#content").load(location.href + " #content");
+}
+
+function reportError(request) {
+   $F('cartResult') = "An error occurred";
+}
+
+</script>
+
+</head>
+
+<body>
+
+<div id="content">
+<p>
+Your Shopping Cart contains:
+<?php
+	$content = $_SESSION['cart'];
+   if(empty($content)){echo "Your cart is currently empty";}
+   	else {
+						$total = 0;
+						echo "<table>";
+						foreach($content as $key=>$value){
+
+						echo "<tr><td>".$value['name']."</td><td align='right'><a href='#' onClick=		\"manageCart('remove',".$key.",".$value['price'].",'".$value['name']."',".$value['stockLeft'].");\"><input type='button' value='-'></a>".$value['quantity'];
+
+						if($value['stockLeft'] > 0){
+						echo "<a href='#' onClick=\"manageCart('add',".$key.",".$value['price'].",'".$value['name']."',".$value['stockLeft'].");\"><input type='button' value='+'></a>";
+						}
+
+						echo "</td><td align='right'>".$value['price']*$value['quantity']."€</td></tr>"; 
+						$total+=$value['price']*$value['quantity'];
+						}
+						echo "<tr><td>Total</td><td colspan='2' align='right'>".$total."€</td></tr></table>"; 
+							 }
+   ?>
+</p>
+<a href="homepage.php">Back to homepage</a>
+
+
+<h2>Add Order To Database </h2>
+			<?php
+			//this info should be taken from cart and from login info and then sent to check.php file 			//to be added to the database.At the moment the data is being inputted manually
+			?>
+		
+			<form action="check.php" method="post">
+			<ul>
+			<li>Email: <input type="text" name="Email"></li>
+			<li>Order_Id: <input type="text" name="Order_Id"></li>
+			<li>Product_items <input type="text" name="Product_itmes"></li>
+			<li>Total_Price <input type="text" name="Total_Price"></li>
+			<li>Delivery_Address: <input type="text" name="Delivery_Address"></li>
+			<li>Date1 <input type="text" name="Date1"></li>
+			<li>Progress <input type="text" name="S_Name"></li>
+			<br>
+			<input type="submit" value="Save">
+			</ul>
+			</form>
+</div>
+</body>
+</html>

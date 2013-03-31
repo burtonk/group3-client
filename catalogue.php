7d8134c7132cb3bba897ee7d055fd9684e2cf28b
Niamh Lawlor <lawlorni@Niamhs-MacBook-Pro.local>
2013-03-31 00:51:41 +0000

    s

--- catalogue.php
+++ catalogue.php
@@ -45,10 +45,12 @@ function reportError(request) {
 						</form>
 					</ul>
 				</div>
+				
+				
 				<div id="login">
 					E-Mail : <input type="text"><br />
 					Password : <input type="text"><br />
-					<input type="button" value="Log in">
+						<input type="button" value="Log in">
 				</div>
 				
 					<?php 
@@ -59,28 +61,28 @@ function reportError(request) {
 						echo "<table>";
 						foreach($content as $key=>$value){
 						
-						echo "<tr><td>".$value['name']."</td><td align='right'><a href='#' onClick=\"manageCart('remove',".$key.",".$value['price'].",'".$value['name']."',".$value['stockLeft'].");\"><input type='button' value='-'></a>".$value['quantity'];
+						echo "<tr><td>".$value['name']."</td><td align='right'><a href='#' onClick=\"manageCart('remove',".$key.",".										  $value['price'].",'".$value['name']."',".$value['stockLeft'].");\"><input type='button' value='-'></a>".												$value['quantity'];
 						
 						if($value['stockLeft'] > 0){
-						echo "<a href='#' onClick=\"manageCart('add',".$key.",".$value['price'].",'".$value['name']."',".$value['stockLeft'].");\"><input type='button' value='+'></a>";
-						}
+								echo "<a href='#' onClick=\"manageCart('add',".$key.",".$value['price'].",'".$value['name']."',".													$value['stockLeft'].");\"><input type='button' value='+'></a>";
+								}
 						
 						echo "</td><td align='right'>".$value['price']*$value['quantity']."€</td></tr>"; 
-						$total+=$value['price']*$value['quantity'];
+								$total+=$value['price']*$value['quantity'];
 						}
+						
 						echo "<tr><td>Total</td><td colspan='2' align='right'>".$total."€</td></tr></table>"; 
 							 }
 					?>
 					
 					<a href="viewcart.php"><input type="button" value="View your cart"></a>
-					
 					<a href="CustomerInfo.php"><input type="button" value="My Information"></a>
 					
 				</div>
-			
+			</div>
 		
 				<div id="cart">
-			</div>
+			
 			<div id="products">
 
 				<
@@ -107,7 +109,7 @@ function reportError(request) {
 					$img = $row['Img_location'];
 					$SName = $row['S_Name'];
 					$stock = $row['Stock_Level'];
-					if($i % 2 == 0){echo "</tr><tr>";}
+					if($i % 3 == 0){echo "</tr><tr>";}
 					echo "<td><p><a href='product.php?id=".$PId.".php'><img src='".$img."' height='100' width='100'></a></p>
 					<p><a href='product.php?id=".$PId.".php'>".$name." <i>(".$SName.")</i></a></p>";
 					if($stock <= 0){echo "<p><font color='red'>Out of stock</font></p>

8dcf3ea6635ae825868f48d1ec7ee9a29e4a153a
Niamh Lawlor <lawlorni@Niamhs-MacBook-Pro.local>
2013-04-01 14:30:44 +0100

    code align

--- catalogue.php
+++ catalogue.php
@@ -13,27 +13,28 @@ header('Content-Type: text/html; charset=utf-8');
 		<title>Customer site</title>
 		<link rel="stylesheet" href="style.css" />
 		
-		<script src="js/prototype.js" language="JavaScript" type="text/javascript"></script>
-		<script src="js/jquery-1.9.1.min.js" language="JavaScript" type="text/javascript"></script>
-<script type="text/javascript" language="JavaScript">
-function manageCart(task,item,price,name,stock) {
-   var url = 'managecart.php';
-   var params = 'task=' + task + '&item=' + item + '&price=' + price + '&name=' + name + '&stock=' + stock;
-   var ajax = new Ajax.Updater(
-	          {success: 'cartResult'},
-              url,
-              {method: 'get', parameters: params, onFailure: reportError});
-			  $("#cart").load(location.href + " #cart");
-			   $("#products").load(location.href + " #products");
-}
+			<script src="js/prototype.js" language="JavaScript" type="text/javascript"></script>
+			<script src="js/jquery-1.9.1.min.js" language="JavaScript" type="text/javascript"></script>
+			<script type="text/javascript" language="JavaScript">
+				function manageCart(task,item,price,name,stock) {
+					var url = 'managecart.php';
+					var params = 'task=' + task + '&item=' + item + '&price=' + price + '&name=' + name + '&stock=' + stock;
+					var ajax = new Ajax.Updater(
+						{success: 'cartResult'},
+							url,
+							{method: 'get', parameters: params, onFailure: reportError});
+							$("#cart").load(location.href + " #cart");
+							$("#products").load(location.href + " #products");
+							}		
 
-function reportError(request) {
-   $F('cartResult') = "An error occurred";
-}
+				function reportError(request) {
+					$F('cartResult') = "An error occurred";
+					}
 
-</script>
-		
+			</script>
 	</head>
+	
+	
 	<body>
 	
 			<div id="top-right">
@@ -45,6 +46,7 @@ function reportError(request) {
 						</form>
 					</ul>
 				</div>
+			
 				
 				
 				
@@ -66,7 +68,7 @@ function reportError(request) {
 				
 				
 				
-				
+					<div id="cart">
 					<?php 
 						$content = $_SESSION['cart'];
 						if(empty($content)){echo "Your cart is currently empty";}
@@ -92,59 +94,60 @@ function reportError(request) {
 					<a href="viewcart.php"><input type="button" value="View your cart"></a>
 					<a href="CustomerInfo.php"><input type="button" value="My Information"></a>
 					
-				</div>
-			</div>
-		
-				<div id="cart">
-			
-			<div id="products">
-
 				
+					</div>
+			</div>
+			
+					<div id="products">
+
 				<table>
 					<tr>
 					<?php
 				
 					// Create connection
-					$con=mysqli_connect("k.tfa.ie","disney","kandy","website");
+						$con=mysqli_connect("k.tfa.ie","disney","kandy","website");
 
 					// Check connection
-					if (mysqli_connect_errno($con)){
-						echo "Failed to connect to MySQL: " . mysqli_connect_error();
+						if (mysqli_connect_errno($con)){
+							echo "Failed to connect to MySQL: " . mysqli_connect_error();
 					}
 
-					$result = mysqli_query($con,"SELECT * FROM product");
+						$result = mysqli_query($con,"SELECT * FROM product");
 					
-					$i=0;
-		
-					while($row = mysqli_fetch_array($result)){
-					$name = $row['Name'];
-					$PId = $row['P_Id'];
-					$price = $row['Price'];
-					$img = $row['Img_location'];
-					$SName = $row['S_Name'];
-					$stock = $row['Stock_Level'];
-					if($i % 2 == 0){echo "</tr><tr>";}
+						$i=0;
 					
-					echo "<td><li class = 'active'><p><a href='product.php?id=".$PId.".php'>
-						  <img src='".$img."' height='100' width='100'></a></li></p>
-						  <p><i><a href='product.php?id=".$PId.".php'></i>
-						  ".$name."
-						  (".$SName.")</a></p>";
-					if($stock <= 0){echo "<p><font color='red'>Out of stock</font></p>
-					<i><p style='text-align:right;'>".$price."€</i></p>";
-					}
-					else{
-					if(!isset($_SESSION['cart'][$PId])){$stockLeft = $stock;
-					}
-					else {$stockLeft = $_SESSION['cart'][$PId]['stockLeft'];
-					}
-					if($stockLeft > 0){
-					echo "<p style='text-align:right'>".$price."€</p>
-					<p><a href='#' onClick='manageCart(\"add\",".$PId.",".$price.",\"".$name."\",".$stockLeft.");'></i><input type='button' value='Add to cart'></a></p></td>";
-					}
-					}
-					$i++;
-					} 
+						while($row = mysqli_fetch_array($result)){
+							$name = $row['Name'];
+							$PId = $row['P_Id'];
+							$price = $row['Price'];
+							$img = $row['Img_location'];
+							$SName = $row['S_Name'];
+							$stock = $row['Stock_Level'];
+							if($i % 2 == 0){echo "</tr><tr>";}
+					
+								echo "<td><li class = 'active'><p><a href='product.php?id=".$PId.".php'>
+								<img src='".$img."' height='100' width='100'></a></li></p>
+								<p><i><a href='product.php?id=".$PId.".php'></i>
+								".$name."
+								(".$SName.")</a></p>";
+					
+							if($stock <= 0){echo "<p><font color='red'>Out of stock</font></p>
+								<i><p style='text-align:right;'>".$price."€</i></p>";
+								}
+								
+							else{
+								if(!isset($_SESSION['cart'][$PId])){$stockLeft = $stock;
+								}
+							else {$stockLeft = $_SESSION['cart'][$PId]['stockLeft'];
+								}
+								if($stockLeft > 0){
+										echo "<p style='text-align:right'>".$price."€</p>
+										<p><a href='#' onClick='manageCart(\"add\",".$PId.",".$price.",\"".$name."\",".$stockLeft.");'>
+										</i><input type='button' value='Add to cart'></a></p></td>";
+								}
+								}
+								$i++;
+								} 
 					mysqli_close($con);
 
 					?>
@@ -155,26 +158,23 @@ function reportError(request) {
 				
 				
 				
-<div id='cssmenu'>
-<ul>
-<li class='active'><a href="homepage.php"><img src="Gradinatas.jpeg"alt="logo" width="160"></a></li> 
-   <li class='active'><a href="homepage.php"><span>Home</span></a></li>
-   <li class='has-sub'><a href='#'><span>Products</span></a>
-      <ul>
-         <li class='has-sub'><a href='#'><span>Cat 1</span></a></li>
-         
-      </ul>
-   </li>
-   <li><a href='#'><span>About</span></a></li>
-   <li class='last'><a href='#'><span>Contact</span></a></li>
-    <li class='last'><a href='#'><span>Reviews</span></a></li>
-</ul>
-</div>
-				</div>
+			<div id='cssmenu'>
+				<ul>
+					<li class='active'><a href="homepage.php"><img src="Gradinatas.jpeg"alt="logo" width="160"></a></li> 
+					<li class='active'><a href="homepage.php"><span>Home</span></a></li>
+					<li class='has-sub'><a href='#'><span>Products</span></a>
+						<ul>
+							<li class='has-sub'><a href='#'><span>Cat 1</span></a></li>
+						</ul>
+					</li>
+					
+					<li><a href='#'><span>About</span></a></li>
+					<li class='last'><a href='#'><span>Contact</span></a></li>
+					<li class='last'><a href='#'><span>Reviews</span></a></li>
+				</ul>
+			</div>
+			
+
 	
-	
-</ul>
-</div>
-		</div>
 	</body>
 </html>

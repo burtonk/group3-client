3b42931d5aacdba4fe801edb202318d6ae8b1d73
Niamh Lawlor <lawlorni@Niamhs-MacBook-Pro.local>
2013-04-01 13:33:09 +0100

    login styling

--- homepage.php
+++ homepage.php
@@ -54,14 +54,9 @@ function reportError(request) {
 			
 				
 				
-				
-				
-				
-				
-				
-					<form class="form-1">
+				<form class="form-1">
 				   <p class="field">
-                   		<input type="text" name="login" placeholder="Username or 								email">
+                   		<input type="text" name="login" placeholder="Username or email">
                    		<i class="icon-user icon-large"></i>
                    </p>
                    
@@ -71,7 +66,7 @@ function reportError(request) {
                    </p>        
                    
                    <p class="submit">
-                   <button type="submit" name="submit"><i class="icon-arrow-right icon-large"></i></button>
+                   		<button type="submit" name="submit"><i class="icon-arrow-right icon-large"></i></button>
                    </p>
                    </form>
 				
@@ -90,16 +85,17 @@ function reportError(request) {
 						echo "<table>";
 						foreach($content as $key=>$value){
 						
-						echo "<tr><td>".$value['name']."</td><td align='right'><a href='#' onClick=\"manageCart('remove',".$key.",".$value['price'].",'".$value['name']."',".$value['stockLeft'].");\"><input type='button' value='-'></a>".$value['quantity'];
+						echo 	"<tr><td>".$value['name']."</td><td align='right'><a href='#' onClick=\"manageCart('remove',".$key.",".											$value['price'].",'".$value['name']."',".$value['stockLeft'].");\"><input type='button' value='-'></a>".										$value['quantity'];
 						
 						if($value['stockLeft'] > 0){
-						echo "<a href='#' onClick=\"manageCart('add',".$key.",".$value['price'].",'".$value['name']."',".$value['stockLeft'].");\"><input type='button' value='+'></a>";
+							echo "<a href='#' onClick=\"manageCart('add',".$key.",".$value['price'].",'".$value['name']."',".												$value['stockLeft'].");\"><input type='button' value='+'></a>";
 						}
 						
-						echo "</td><td align='right'>".$value['price']*$value['quantity']."€</td></tr>"; 
-						$total+=$value['price']*$value['quantity'];
+						echo 	"</td><td align='right'>".$value['price']*$value['quantity']."€</td></tr>"; 
+								$total+=$value['price']*$value['quantity'];
 						}
-						echo "<tr><td>Total</td><td colspan='2' align='right'>".$total."€</td></tr></table>"; 
+						
+						echo 	"<tr><td>Total</td><td colspan='2' align='right'>".$total."€</td></tr></table>"; 
 							 }
 					?>
 					<a href="viewcart.php"><input type="button" value="View your cart"></a>
@@ -108,26 +104,22 @@ function reportError(request) {
 			
 						
 		<div id='cssmenu'>
-<ul>
-<li class='active'><a href="homepage.php"><img src="Gradinatas.jpeg"alt="logo" width="160"></a></li> 
-   <li class='active'><a href='homepage.php'><span>Home</span></a></li>
-   <li class='has-sub'><a href='#'><span>Products</span></a>
-      <ul>
-         <li class='has-sub'><a href="catalogue.php"><span>Cat 1</span></a></li>
+			<ul>
+				<li class='active'><a href="homepage.php"><img src="Gradinatas.jpeg"alt="logo" width="160"></a></li> 
+				<li class='active'><a href='homepage.php'><span>Home</span></a></li>
+				<li class='has-sub'><a href='#'><span>Products</span></a>
+			<ul>
+				
+				<li class='has-sub'><a href="catalogue.php"><span>Cat 1</span></a></li>
          
-      </ul>
-   </li>
-   <li><a href='#'><span>About</span></a></li>
-   <li class='last'><a href='#'><span>Contact</span></a></li>
-    <li class='last'><a href='#'><span>Reviews</span></a></li>
-</ul>
-</div>	
-<div id="home">
+			</ul>
+			
+			</li>
+   				<li><a href='#'><span>About</span></a></li>
+   				<li class='last'><a href='#'><span>Contact</span></a></li>
+   				<li class='last'><a href='#'><span>Reviews</span></a></li>
+   		</ul>
+   		</div>	
 
-
-
-
-
-</div>
 </body>
 </html>

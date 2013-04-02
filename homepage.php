3d042c95632aaab01472bfe672de6ddd1520ada4
Niamh Lawlor <lawlorni@Niamhs-MacBook-Pro.local>
2013-04-02 15:19:36 +0100

    menu

--- homepage.php
+++ homepage.php
@@ -101,12 +101,14 @@ function reportError(request) {
 						
 		<div id='cssmenu'>
 			<ul>
-				<li class='active'><a href="homepage.php"><img src="Gradinatas.jpeg"alt="logo" width="160"></a></li> 
-					<li class='active'><a href="homepage.php"><span>Home</span></a></li>
-					<li class='has-sub'><a href='catalogue.php'><span>Herbs & Spices</span></a></li>
-					<li><a href='#'><span>About</span></a></li>
-					<li class='last'><a href='#'><span>Contact</span></a></li>
-					<li class='last'><a href='#'><span>Reviews</span></a></li>
+			<tr>
+				<td class='active'><a href="homepage.php"><img src="Gradinatas.jpeg"alt="logo" width="160"></a></td>
+				<td class='active'><a href="homepage.php"><span>Home</span></a></td>
+					<td class='has-sub'><a href='catalogue.php'><span>Herbs & Spices</span></a></td>
+					<td><a href='#'><span>About</span></a></td>
+					<td class='last'><a href='#'><span>Contact</span></a></td>
+					<td class='last'><a href='#'><span>Reviews</span></a></td>
+			</tr>
    		</ul>
    		</div>	
 

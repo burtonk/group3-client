fd15d3ffe3355dfa13ff4adf434b2c9f662ea7cb
burtonk <burtonk@tcd.ie>
2013-04-03 12:13:07 +0200

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -35,7 +35,7 @@ function reportError(request) {
 
 		
    		<div id="hlogo">
-   		<a href="homepage.php"><img src="Gradinatas.jpeg"alt="logo" width="210" height="200"></a>
+   		<a href="homepage.php"><img src="http://k.tfa.ie/pics/logo.gif"alt="logo" width="210" height="200"></a>
    		</div>
 		
 		

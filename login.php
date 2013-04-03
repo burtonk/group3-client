c0da49f9ba2a4085b686a926ffc365d970a0494c
burtonk <burtonk@tcd.ie>
2013-04-03 12:48:58 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,58 +1,4 @@
-<?php
-  // Connects to your Database
-
- mysql_connect("k.tfa.ie", "disney", "kandy") or die(mysql_error());
-
- mysql_select_db("website") or die(mysql_error());
-
- //Checks if there is a login cookie
-
- if(isset($_COOKIE['ID_my_site']))
-
-
- //if there is, it logs you in and directes you to the members page
-
- { 
- 	$username = $_COOKIE['ID_my_site']; 
-
- 	$pass = $_COOKIE['Key_my_site'];
-
- 	 	$check = mysql_query("SELECT * FROM the_user WHERE Email = '$username' ")or die(mysql_error());
-
- 	while($info = mysql_fetch_array( $check )) 	
-
- 		{
-
- 		if ($pass != $info['Password']) 
-
- 			{
-                       //if user doesn't match
-			header("Location: homepage.php");
-
- 			 			}
-
- 		else
-
- 			{
-			//do nothing
-
-                         //if everything is ok... 
-
- 			}
-
- 		}
-
- }
- else {
-    header("Location: login.php");
-
-  }
-?>
-
-***************************************************************************************************
-This needs to be called login.php and goes in the normal folder. 
-***************************************************************************************************
- <?php 
+<?php 
 
  // Connects to your Database 
 
@@ -62,59 +8,6 @@ This needs to be called login.php and goes in the normal folder.
 
 
  //Checks if there is a login cookie
-
- if(isset($_COOKIE['ID_my_site']))
-
-
- //if there is, it logs you in and directes you to the members page
-
- { 
- 	$username = $_COOKIE['ID_my_site']; 
-
- 	$pass = $_COOKIE['Key_my_site'];
-
- 	 	$check = mysql_query("SELECT * FROM the_user WHERE Email = '$username'")or die(mysql_error());
-
- 	while($info = mysql_fetch_array( $check )) 	
-
- 		{
-
- 		if ($pass != $info['Password']) 
-
- 			{
-
- 			 			}
-
- 		else
- 			{
-
-		// all happy
-
-
- 			}
-
- 		}
-
- }
-
-
- //if the login form is submitted 
-
- if (isset($_POST['submit'])) { // if form has been submitted
-
-
-
- // makes sure they filled it in
-
- 	if(!$_POST['username'] | !$_POST['pass']) {
-
- 		die('You did not fill in a required field.');
-
- 	}
-
- 	// checks it against the database
-
-
  	if (!get_magic_quotes_gpc()) {
 
  		$_POST['email'] = addslashes($_POST['email']);
@@ -133,7 +26,7 @@ This needs to be called login.php and goes in the normal folder.
 
  		die('That user does not exist in our database.');
 
- 				}
+}
 
  while($info = mysql_fetch_array( $check )) 	
 
@@ -169,11 +62,7 @@ This needs to be called login.php and goes in the normal folder.
 
  setcookie(Key_my_site, $_POST['pass'], $hour);	 
 
- 
 
- //then redirect them to the members area 
-
- header("Location: home.php"); 
 
  } 
 
@@ -181,44 +70,3 @@ This needs to be called login.php and goes in the normal folder.
 
  } 
 
- else 
-
-{	 
-
- 
-
- // if they are not logged in 
-
- ?> 
-
- <form action="<?php echo $_SERVER['PHP_SELF']?>" method="post"> 
-
- <table border="0"> 
-
- <tr><td colspan=2><h1>Login</h1></td></tr> 
-
- <tr><td>Email:</td><td> 
-
- <input type="text" name="username" maxlength="40"> 
-
- </td></tr> 
-
- <tr><td>Password:</td><td> 
-
- <input type="password" name="pass" maxlength="50"> 
-
- </td></tr> 
-
- <tr><td colspan="2" align="right"> 
-
- <input type="submit" name="submit" value="Login"> 
-
- </td></tr> 
-
- </table> 
-
- </form> 
-
- <?php 
-
- } 

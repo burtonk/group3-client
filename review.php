83879a7277a10b56b463607d4007f4ed8876f834
AlexDouard <al.douard@gmail.com>
2013-04-03 13:14:36 +0200

    Update review.php

--- review.php
+++ review.php
@@ -9,7 +9,7 @@ include "config/details.php";
 						echo "Failed to connect to MySQL: " . mysqli_connect_error();
 					}
 
-					$result = mysqli_query($con,"INSERT INTO reviews(Email,Name,Stars,Comments,Date1,Time1,Product_Id) VALUES(\"".$_POST['email']."\",\"".$_POST['name']."\"".$_POST['rating'].",\"".$_POST['comment']."\",CURDATE(),CURTIME(),".$_POST['id'].")");
-					//header("Location: product.php?id=".$_POST['id']);
-					echo "INSERT INTO reviews(Email,Name,Stars,Comments,Date1,Time1,Product_Id) VALUES(\"".$_POST['email']."\",\"".$_POST['name']."\"".$_POST['rating'].",\"".$_POST['comment']."\",CURDATE(),CURTIME(),".$_POST['id'].")";
+					$result = mysqli_query($con,"INSERT INTO reviews(Email,Name,Stars,Comments,Date1,Time1,Product_Id) VALUES(\"".$_POST['email']."\",\"".$_POST['name']."\",".$_POST['rating'].",\"".$_POST['comment']."\",CURDATE(),CURTIME(),".$_POST['id'].")");
+					/eader("Location: product.php?id=".$_POST['id']);
+					///echo "INSERT INTO reviews(Email,Name,Stars,Comments,Date1,Time1,Product_Id) VALUES(\"".$_POST['email']."\",\"".$_POST['name']."\"".$_POST['rating'].",\"".$_POST['comment']."\",CURDATE(),CURTIME(),".$_POST['id'].")";
 ?>

0720b9f529d0372e099050f93ae7f38e1a14ebc6
burtonk <burtonk@tcd.ie>
2013-04-03 12:25:15 +0200

    Update about.php

--- about.php
+++ about.php
@@ -112,7 +112,7 @@ function reportError(request) {
    			
 	   		<td><a href="homepage.php"><span>Home</span></a></td>
 	   		<td><a href="catalogue.php"><span>Herbs & Spices</span></a></td>
-	   		<td><a href="#"><span>Contact</span></a></td>
+	   		<td><a href="contact.php"><span>Contact</span></a></td>
 	   		<td><a href="about.php"><span>About</span></a></td>
 	   		<td><a href="CompanyReviews.php"><span>Reviews</span></a></td>
 	   		</tr>
